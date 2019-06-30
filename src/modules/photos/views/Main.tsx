e839d4411a8a5233713ca5c32be8ff7a92b10c9a
wooline <wooline@qq.com>
2019-06-30 20:07:04 +0800

    init project

---
+++ src/modules/photos/views/Main.tsx
@@ -0,0 +1,17 @@
+import {Text, View} from 'react-native';
+
+import React from 'react';
+
+interface Props {}
+
+class Component extends React.PureComponent<Props> {
+  public render() {
+    return (
+      <View>
+        <Text>photos2</Text>
+      </View>
+    );
+  }
+}
+
+export default Component;
